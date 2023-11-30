a546c74ae5162b28d60a7386b72987bbd05fcf61
DESKTOP-GHGQ78S\vaibh <vvga0123@gmail.com>
2023-12-01 02:26:25 +0530

    added login page;

--- src/main/java/com/marcobehler/myfancypdfinvoices/web/InvoicesController.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/web/InvoicesController.java
@@ -3,10 +3,14 @@ package com.marcobehler.myfancypdfinvoices.web;
 import com.marcobehler.myfancypdfinvoices.dto.InvoiceDto;
 import com.marcobehler.myfancypdfinvoices.model.Invoice;
 import com.marcobehler.myfancypdfinvoices.service.InvoiceService;
+import com.marcobehler.myfancypdfinvoices.web.forms.LoginForm;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -19,7 +23,9 @@ public class InvoicesController {
     }
 
     @GetMapping("/")
-    public String getIndexPage(){
+    public String getIndexPage(Model model, @RequestParam(required = false, defaultValue = "Stranger") String username) {
+        model.addAttribute("username", username);
+        model.addAttribute("currentDate", LocalDateTime.now());
         return "index.html";
     }
 
@@ -31,5 +37,22 @@ public class InvoicesController {
     @PostMapping("/invoices")
     public Invoice createInvoice(@RequestBody @Valid InvoiceDto invoiceDto) {
         return invoiceService.create(invoiceDto.getUserId(), invoiceDto.getAmount());
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("loginForm", new LoginForm());
+        return "login.html";
+    }
+    @PostMapping("/login")
+    public String login(@ModelAttribute @Valid LoginForm loginForm, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()) {
+            return "login.html";
+        }
+        if (loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        model.addAttribute("invalidCredentials", "true");
+        return "login.html";
     }
 }
